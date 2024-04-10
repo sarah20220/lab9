e7e7a4ee9fc70a68aa9baf0a3d469fe802c1ae62
njneered <n.lacambra@ufl.edu>
2024-04-09 23:13:25 -0400

    added functions for encode and decode menu options

---
+++ hello.py
@@ -0,0 +1,19 @@
+# function for encode option
+def encoder(password):
+	# new string
+	encoded = ""
+	# loop to iterate through each character in string
+	for i in password:
+		shift_up = str((int(i) + 3))
+		encoded += shift_up
+	return encoded
+
+# function for decode option
+def decode(encoded):
+	# new string
+	decoded = ""
+	# loop to iterate through each character in the string
+	for i in encoded:
+		shift_down = str((int(i) - 3))
+		decoded += shift_down
+	return decoded
